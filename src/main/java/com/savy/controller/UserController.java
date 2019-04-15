a3319fe3013f97cb13330bea19fdb81332f6f6e3
limbokobe <45932724+limbokobe@users.noreply.github.com>
2019-04-15 20:49:55 +0800

    注册接口

--- src/main/java/com/savy/controller/UserController.java
+++ src/main/java/com/savy/controller/UserController.java
@@ -3,9 +3,14 @@ package com.savy.controller;
 import com.savy.model.User;
 import com.savy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.awt.*;
 
 @RequestMapping(value = "/user")
 @Controller
@@ -29,6 +34,13 @@ public class UserController {
         User user = userService.getUserById(1);
         System.out.println(user.toString());
         return user;
+    }
+    @RequestMapping(value = "/register",method = {RequestMethod.POST},produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public Integer register(@RequestParam  String userName,@RequestParam String password,@RequestParam String sex,@RequestParam String phone,@RequestParam String email){
+        System.out.println("call /user/register");
+        Integer r=userService.insertUser(userName,password,0,"普通用户",sex,phone,email);
+        return r;
     }
 
 }
